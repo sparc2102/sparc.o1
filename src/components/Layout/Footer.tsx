3503e1628cb9e1f18edaa7f5e3d5d7b0561b1149
sparc2102 <ansa37013@gmail.com>
2025-09-21 04:00:28 +0000

    your new changes

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -71,7 +71,7 @@ export function Footer() {
             <div className="space-y-3">
               <div className="flex items-center space-x-2">
                 <Mail className="h-4 w-4 text-blue-400" />
-                <span className="text-gray-300">info@sparc-pharma.org</span>
+                <span className="text-gray-300">zanesparc@gmail.com</span>
               </div>              
               <div className="flex items-center space-x-2">
                 <MapPin className="h-4 w-4 text-blue-400" />
